078430789b9e6e0621d03742e3d1c582aca5ff28
RomanW04 <154457259+RomanW04@users.noreply.github.com>
2024-03-26 16:46:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mon Site Web React</h1>
+        <p>Bienvenue sur mon site!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
